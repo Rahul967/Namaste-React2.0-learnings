73f1b6b71a39bfd2f34d10354cab14f99b88984c
Rahul Kumar Singh <contactrahulkrsingh@gmail.com>
2024-01-04 11:40:06 +0530

    some basic things in react

--- App.js
+++ App.js
@@ -1,1 +1,9 @@
 // hi this is the first commit 
+// const heading = React.createElement("h1",{},"hello world from React");
+// const root = reactDOM.createRoot.(document.getElementById("root"));
+// root.render(heading);
+
+const heading = document.createElement("h1");
+heading.innerHTML = "Hello world from javascript"
+const root = document.getElementById("root");
+root.appendChild(heading);
